571cd8e001bc648020438d820000482d9be09fc6
sohee.lee <sohee.lee@rowan.kr>
2023-04-25 12:42:13 +0900

    feat : index.html 응답 구현
    - styles.css 포함

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,6 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -25,18 +22,41 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(isr);
+            String[] request = br.readLine().split(" ");
+
+            String uri = request[1];
+
+            byte[] body = null;
+            String contentType = null;
+            FileInputStream responseBodyStream = null;
+            if(uri.contains("/index.html")){
+                responseBodyStream = new FileInputStream("webapp/index.html");
+
+                contentType = "text/html";
+            }
+
+            if(uri.contains("/css/styles.css")){
+                responseBodyStream = new FileInputStream("webapp/css/styles.css");
+
+                contentType = "text/css";
+            }
+
+            body = responseBodyStream.readAllBytes();
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: "+contentType+";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
