d5ecea279128402edadd026b382e910de1d1617a
sohee.lee <sohee.lee@rowan.kr>
2023-05-04 11:24:58 +0900

    refactor : method enum ▶️ String으로 변경

--- src/main/java/data/Uri.java
+++ src/main/java/data/Uri.java
@@ -1,32 +1,29 @@
 package data;
 
-import annotation.RequestMapping.RequestMethod;
-
 import java.util.Arrays;
-
-import static annotation.RequestMapping.RequestMethod.*;
 
 public enum Uri {
 
-    INDEX(GET, "/index.html", "webapp/index.html", "text/html"),
-    STYLES(GET,"/css/styles.css", "webapp/css/styles.css", "text/css"),
-    BOOTSTRAP_CSS(GET,"/css/bootstrap.min.css", "webapp/css/bootstrap.min.css", "text/css"),
-    BOOTSTRAP_JS(GET,"/js/bootstrap.min.js", "webapp/js/bootstrap.min.js", "text/javascript"),
-    JQUERY(GET,"/js/jquery-2.2.0.min.js", "webapp/js/jquery-2.2.0.min.js", "text/javascript"),
-    SCRIPT(GET,"/js/scripts.js", "webapp/js/scripts.js", "text/javascript"),
-    FAVICON(GET,"/favicon.ico", "webapp/favicon.ico", "image/x-icon"),
-    FORM(GET,"/user/form.html","webapp/user/form.html", "text/html"),
+    INDEX("GET", "/index.html", "webapp/index.html", "text/html"),
+    STYLES("GET","/css/styles.css", "webapp/css/styles.css", "text/css"),
+    BOOTSTRAP_CSS("GET","/css/bootstrap.min.css", "webapp/css/bootstrap.min.css", "text/css"),
+    BOOTSTRAP_JS("GET","/js/bootstrap.min.js", "webapp/js/bootstrap.min.js", "text/javascript"),
+    JQUERY("GET","/js/jquery-2.2.0.min.js", "webapp/js/jquery-2.2.0.min.js", "text/javascript"),
+    SCRIPT("GET","/js/scripts.js", "webapp/js/scripts.js", "text/javascript"),
+    FAVICON("GET","/favicon.ico", "webapp/favicon.ico", "image/x-icon"),
+    FORM("GET","/user/form.html","webapp/user/form.html", "text/html"),
 
-    JOIN(GET,"/user/create", null, "text/html"),
-    DEFAULT(GET,"/", null, "text/html"),
+    JOIN("GET","/user/create", null, "text/html"),
+    JOIN_POST("POST","/user/create", null, "text/html"),
+    DEFAULT("GET","/", null, "text/html"),
     ;
 
-    RequestMethod method;
+    String method;
     String uri;
     String path;
     String contentType;
 
-    Uri(RequestMethod method, String uri, String path, String contentType) {
+    Uri(String method, String uri, String path, String contentType) {
         this.method = method;
         this.uri = uri;
         this.path = path;
@@ -37,7 +34,7 @@ public enum Uri {
         return this.uri;
     }
 
-    public RequestMethod getMethod(){
+    public String getMethod(){
         return this.method;
     }
 
@@ -52,7 +49,7 @@ public enum Uri {
     public static Uri findResponseInfo(String method, String uri){
         return Arrays.stream(values())
                 .filter(value -> value.uri.contains(uri)
-                        && value.getMethod() == findMethod(method))
+                        && value.getMethod().equals(method))
                 .findFirst().orElse(DEFAULT);
     }
 
