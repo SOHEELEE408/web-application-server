bf27354f6a2aeef3eeb9f9196fbdcdacb9906d3f
sohee.lee <sohee.lee@rowan.kr>
2023-05-19 08:58:35 +0900

    test : 회원가입(GET/POST) 테스트 코드 추가

---
+++ src/test/java/data/HttpRequestTest.java
@@ -0,0 +1,50 @@
+package data;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpRequestTest {
+
+    private String testDirectory = "./src/test.resources/";
+
+    @Test
+    @DisplayName("")
+    void request_GET() throws Exception {
+        // given
+        InputStream in = new FileInputStream(new File(testDirectory+"Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        // when
+        assertEquals("GET", request.getMethod());
+        assertEquals("/user/create", request.getUrl());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("javajigi", request.getParammeter("userId"));
+
+        // then
+
+    }
+
+    @Test
+    @DisplayName("")
+    void request_POST() throws Exception {
+        // given
+        InputStream in = new FileInputStream(new File(testDirectory+"Http_POST.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        // when
+        assertEquals("POST", request.getMethod());
+        assertEquals("/user/create", request.getUrl());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("javajigi", request.getParammeter("userId"));
+
+        // then
+    }
+
+}
