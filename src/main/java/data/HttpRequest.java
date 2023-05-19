aab829733856e3941b80025ba29ea89a8c13679d
sohee.lee <sohee.lee@rowan.kr>
2023-05-19 10:11:32 +0900

    refactor : step-1) 테스트 코드 기반 HttpRequest 리팩토링

--- src/main/java/data/HttpRequest.java
+++ src/main/java/data/HttpRequest.java
@@ -2,66 +2,76 @@ package data;
 
 import util.HttpRequestUtils;
 import util.IOUtils;
-import webserver.RequestHandler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static webserver.RequestHandler.*;
 
 public class HttpRequest {
 
+    private Map<String, String> header = new HashMap<>();
     private String method;
-    private String url;
-    private int contentLength;
-    private boolean logined;
-    private Map<String, String> params = new HashMap<>();
+    private String path;
+    private Map<String, String> parameter = new HashMap<>();
 
+
+    public String getHeader(String key){
+        return header.get(key);
+    }
 
     public String getMethod() {
         return method;
     }
 
-    public String getUrl() {
-        return url;
+    public String getPath() {
+        return path;
     }
 
-    public int getContentLength() {
-        return contentLength;
+    public String getParameter(String key){
+        return parameter.get(key);
     }
 
-    public boolean isLogined() {
-        return logined;
-    }
+    public HttpRequest(InputStream in){
 
-    public Map<String, String> getParams() {
-        return params;
-    }
-
-    public HttpRequest(BufferedReader br){
         try{
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
             String line = br.readLine();
             String[] tokens = line.split(" ");
             method = tokens[0];
 
-            while (!line.equals("")) {
-                log.debug("header : {}", line);
+            while (line != null) {
+
                 line = br.readLine();
+                if(line == null) break;
 
-                if (line.contains("Content-Length"))
-                    contentLength = RequestHandler.getContentLength(line);
+                log.debug("header : {}", line);
+                String[] headers = line.split(":");
 
-                if (line.contains("Cookie"))
-                    logined = isLogin(line);
+                header.put(headers[0], headers[1].trim());
             }
 
-            url = tokens[1];
-            String body = IOUtils.readData(br, contentLength);
-            params = HttpRequestUtils.parseQueryString(body);
+            String body = null;
+            if(method.equals("GET")){
+                String[] paths = tokens[1].split("\\?");
+                path = paths[0];
 
+                body = paths[1];
+            }
+
+            if(method.equals("POST")){
+                path = tokens[1];
+
+                body = IOUtils.readData(br, Integer.parseInt(header.get("Content-Length")));
+            }
+
+            parameter = HttpRequestUtils.parseQueryString(body);
 
         } catch (IOException e) {
             log.error(e.getMessage());
