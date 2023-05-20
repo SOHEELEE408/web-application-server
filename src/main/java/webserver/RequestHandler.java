17813fc98cdb15af347a794ed4d10ea9bc002f8d
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-05-20 19:54:30 +0900

    refactor : 요청 처리 분기문 리팩토링을 통한 로직 반복 줄임

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,17 +2,12 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.util.Collection;
-import java.util.Map;
 
+import config.WebConfiguration;
 import data.HttpRequest;
 import data.HttpResponse;
-import db.DataBase;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
 
 
 public class RequestHandler extends Thread {
@@ -33,54 +28,10 @@ public class RequestHandler extends Thread {
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
 
-            if ("/user/create".equals(request.getPath())) {
-
-                User user = new User(request.getParameter("userId"), request.getParameter("password"),
-                        request.getParameter("name"), request.getParameter("email"));
-                DataBase.addUser(user);
-
-                response.sendRedirect("/index.html");
-
-            } else if ("/user/login".equals(request.getPath())) {
-                User user = DataBase.findUserById(request.getParameter("userId"));
-
-                if (user == null) {
-                    response.forward("/user/login_failed.html");
-                    return;
-                }
-
-                if (user.getPassword().equals(request.getParameter("password"))) {
-
-                    response.addHeader("Set-Cookie", "logined=true");
-                    response.sendRedirect("/index.html");
-
-                } else {
-                    response.forward("/user/login_failed.html");
-                }
-
-            } else if("/user/list".equals(request.getPath())) {
-                if(!Boolean.parseBoolean(request.getCookies("logined"))) {
-                    response.forward("/user/login.html");
-                    return;
-                }
-
-                Collection<User> users = DataBase.findAll();
-                StringBuilder sb = new StringBuilder();
-                sb.append("<table border = '1'>");
-                for (User user : users){
-                    sb.append("<tr>");
-                    sb.append("<td>"+ user.getUserId()+"/<td>");
-                    sb.append("<td>"+ user.getName()+"/<td>");
-                    sb.append("<td>"+ user.getEmail()+"/<td>");
-                    sb.append("</tr>");
-                }
-                sb.append("</table>");
-                response.setBody(null, sb.toString().getBytes());
-                response.forward("/user/list.html");
-
-            } else {
-                response.forward(request.getPath());
-            }
+            if(request.getPath().contains("."))
+                WebConfiguration.controller().get("static").service(request, response);
+            else
+                WebConfiguration.controller().get(request.getPath()).service(request, response);
 
         } catch (IOException e) {
             log.error(e.getMessage());
