7a515065e635446ecc70463940377b154a709a89
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-05-21 19:27:34 +0900

    refactor : 교재 코드 적용을 위해 step-2-httpResponse merge 후 충돌 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,12 +2,10 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Collection;
 
+import config.WebConfiguration;
 import data.HttpRequest;
 import data.HttpResponse;
-import db.DataBase;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,53 +29,10 @@ public class RequestHandler extends Thread {
             String path = getDefaultPath(request.getPath());
             HttpResponse response = new HttpResponse(out);
 
-            if ("/user/create".equals(path)) {
-
-                User user = new User(request.getParameter("userId"), request.getParameter("password"),
-                        request.getParameter("name"), request.getParameter("email"));
-                DataBase.addUser(user);
-
-                response.sendRedirect("/index.html");
-
-            } else if ("/user/login".equals(request.getPath())) {
-                User user = DataBase.findUserById(request.getParameter("userId"));
-
-                if (user != null) {
-                    if (user.getPassword().equals(request.getParameter("password"))) {
-
-                        response.addHeader("Set-Cookie", "logined=true");
-                        response.sendRedirect("/index.html");
-
-                    } else {
-                        response.forward("/user/login_failed.html");
-                    }
-                } else {
-                    response.sendRedirect("/user/login_failed.html");
-                }
-
-            } else if("/user/list".equals(path)) {
-
-                if(!request.isLogin()) {
-                    response.forward("/user/login.html");
-                    return;
-                }
-
-                Collection<User> users = DataBase.findAll();
-                StringBuilder sb = new StringBuilder();
-                sb.append("<table border = '1'>");
-                for (User user : users){
-                    sb.append("<tr>");
-                    sb.append("<td>"+ user.getUserId()+"/<td>");
-                    sb.append("<td>"+ user.getName()+"/<td>");
-                    sb.append("<td>"+ user.getEmail()+"/<td>");
-                    sb.append("</tr>");
-                }
-                sb.append("</table>");
-                response.forwardBody(sb.toString());
-
-            } else {
-                response.forward(request.getPath());
-            }
+            if(request.getPath().contains("."))
+                WebConfiguration.controller().get("static").service(request, response);
+            else
+                WebConfiguration.controller().get(request.getPath()).service(request, response);
 
         } catch (IOException e) {
             log.error(e.getMessage());
