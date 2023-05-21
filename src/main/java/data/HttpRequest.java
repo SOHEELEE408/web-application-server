91b28e0d5da3001e71fef4cd99922cef810190e8
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-05-21 17:42:25 +0900

    refactor : [withBook] 상태 라인 처리 메소드 RequestLine 클래스로 분리
    - 테스트하기 까다로운 메소드는 클래스로 분리해보자
    - getter는 단순하게 멤버 필드만 반환하지 않는다.

--- src/main/java/data/HttpRequest.java
+++ src/main/java/data/HttpRequest.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
+import util.RequestLine;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,8 +17,7 @@ import java.util.Map;
 public class HttpRequest {
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
 
-    private String method;
-    private String path;
+    private RequestLine requestLine;
     private Map<String, String> parameter = new HashMap<>();
     private Map<String, String> header = new HashMap<>();
 
@@ -27,11 +27,11 @@ public class HttpRequest {
     }
 
     public String getMethod() {
-        return method;
+        return requestLine.getMethod();
     }
 
     public String getPath() {
-        return path;
+        return requestLine.getPath();
     }
 
     public String getParameter(String key){
@@ -47,7 +47,7 @@ public class HttpRequest {
             String line = br.readLine();
             if(line == null) return;
 
-            processRequestLine(line);
+            requestLine = new RequestLine(line);
 
             line = br.readLine(); // 요청 라인과 header 사이의 공백
             while (line != null && !line.equals("")) {
@@ -58,33 +58,14 @@ public class HttpRequest {
                 line = br.readLine();
             }
 
-            if("POST".equals(method)){
+            if("POST".equals(getMethod())){
                 String body = IOUtils.readData(br, Integer.parseInt(header.get("Content-Length")));
                 parameter = HttpRequestUtils.parseQueryString(body);
-            }
+            } else
+                parameter = requestLine.getParameter();
 
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-
-    private void processRequestLine(String requestLine) {
-        log.debug("request line: {}", requestLine);
-
-        String[] tokens = requestLine.split(" ");
-        method = tokens[0];
-
-        if ("POST".equals(method)){
-            path = tokens[1];
-            return;
-        }
-
-        int index = tokens[1].indexOf("?");
-        if(index == -1) path = tokens[1];
-        else {
-            path = tokens[1].substring(0, index);
-            parameter = HttpRequestUtils.parseQueryString(tokens[1].substring(index+1));
-        }
-
     }
 }
