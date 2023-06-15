dc43cc760131bd5b836a9469f266ceac5b8a5b08
sohee.lee <sohee.lee@rowan.kr>
2023-06-15 10:59:48 +0900

    feat : 세션 조회, 삭제 메서드 추가
    - Map에서 바로 조회하지 않음
    - 요청된 id에 해당하는 세션이 없을 경우, 같은 아이디로 세션 생성 및 저장

--- src/main/java/data/HttpSessions.java
+++ src/main/java/data/HttpSessions.java
@@ -7,4 +7,18 @@ public class HttpSessions {
 
     public static Map<String, HttpSession> httpSession = new LinkedHashMap<>();
 
+    public static HttpSession getSession(String id){
+        HttpSession session = httpSession.get(id);
+
+        if(session == null){
+            session = new HttpSession(id);
+            httpSession.put(session.getId(), session);
+        }
+
+        return session;
+    }
+
+    static void remove(String id){
+        httpSession.remove(id);
+    }
 }
