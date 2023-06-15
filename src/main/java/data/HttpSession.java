4b656ffa9da0766eccb72d033098d3365c004fac
sohee.lee <sohee.lee@rowan.kr>
2023-06-15 09:55:20 +0900

    feat : 사용자 정보를 저장할 세션 & 세션들을 보관하는 클래스 생성

---
+++ src/main/java/data/HttpSession.java
@@ -0,0 +1,35 @@
+package data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class HttpSession {
+
+    private String id;
+    private Map<String, Object> attributes = new HashMap<>();
+
+    public HttpSession(){
+        this.id = String.valueOf(UUID.randomUUID());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Object getAttribute(String name){
+        return attributes.get(name);
+    }
+
+    public void setAttributes(String name, Object value){
+        attributes.put(name, value);
+    }
+
+    public void removeAttribute(String name){
+        attributes.remove(name);
+    }
+
+    public void invalidate() {
+        attributes.clear();
+    }
+}
---
+++ src/main/java/data/HttpSessions.java
@@ -0,0 +1,10 @@
+package data;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class HttpSessions {
+
+    public static Map<String, HttpSession> httpSession = new LinkedHashMap<>();
+
+}
